7781b7c0767261222b2989e8bd44683a1f334251
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2023-06-22 20:41:44 +0530

    added date

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -81,7 +81,7 @@ export default function Home() {
             const response = await fetch("https://ipapi.co/json/");
             const data = await response.json();
             const { ip } = data;
-            console.log(formState, Date.now(), ip);
+            console.log(formState, ip);
           }}
         >
           <InputBox
