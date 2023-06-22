a7ee18f85f474c1aa325111e27ce0179960a67ce
AniruddhaGawali <73152642+AniruddhaGawali@users.noreply.github.com>
2023-06-22 22:18:31 +0530

    new: connect api to database

--- src/app/api/getData/route.js
+++ src/app/api/getData/route.js
@@ -1,20 +1,18 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+// eslint-disable-next-line import/prefer-default-export
 export async function GET() {
-    try{
-        console.log("running");
-
-        const result = await prisma.student.findMany();
-        
-        return new NextResponse(JSON.stringify(result), {
-            headers: {
-                'content-type': 'application/json',
-            },
-        });
-    }catch(error){
-        return new NextResponse('Internal Server Error', { status: 500 });
-    }
+  try {
+    const result = await prisma.student.findMany();
+    return new NextResponse(JSON.stringify(result), {
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+  } catch (error) {
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
